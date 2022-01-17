e400615e4b8af39cd2cf59ebedabb084405aa981
khaoulagharib98 <khaoulagharib98@gmail.com>
2022-01-17 22:28:28 +0100

    reintCommit

---
+++ src/main/java/com/example/gestioncontrat/GestionContratApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestioncontrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionContratApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionContratApplication.class, args);
+    }
+
+}
