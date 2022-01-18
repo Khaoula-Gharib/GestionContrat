895b1a3e71b3b401c023410dcda0a06ceedd7f3b
TIKOU Jihane <tikoujihane@gmail.com>
2022-01-18 18:01:18 +0100

    commit clientDao update

--- src/main/java/com/example/gestioncontrat/dao/ClientDao.java
+++ src/main/java/com/example/gestioncontrat/dao/ClientDao.java
@@ -1,9 +1,9 @@
 package com.example.gestioncontrat.dao;
 
-import com.example.gestioncontrat.bean.client;
+import com.example.gestioncontrat.bean.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface clientDao extends JpaRepository<client,Long> {
+public interface ClientDao extends JpaRepository<Client,Long> {
 }
