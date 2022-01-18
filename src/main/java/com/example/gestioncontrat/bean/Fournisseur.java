32394299b516214a1122d15cdd961a27d94b92ef
khaoulagharib98 <khaoulagharib98@gmail.com>
2022-01-18 16:06:55 +0100

    creation de la classe Fournisseur

---
+++ src/main/java/com/example/gestioncontrat/bean/Fournisseur.java
@@ -0,0 +1,77 @@
+package com.example.gestioncontrat.bean;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Fournisseur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String adresse;
+    private Long numTel;
+    private Long numCompte;
+    @OneToMany(mappedBy = "fournisseur")
+    private List<Contrat> contrats;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public Long getNumTel() {
+        return numTel;
+    }
+
+    public void setNumTel(Long numTel) {
+        this.numTel = numTel;
+    }
+
+    public Long getNumCompte() {
+        return numCompte;
+    }
+
+    public void setNumCompte(Long numCompte) {
+        this.numCompte = numCompte;
+    }
+
+    public List<Contrat> getContrats() {
+        return contrats;
+    }
+
+    public void setContrats(List<Contrat> contrats) {
+        this.contrats = contrats;
+    }
+
+    @Override
+    public String toString() {
+        return "Fournisseur{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", numTel=" + numTel +
+                ", numCompte=" + numCompte +
+                ", contrats=" + contrats +
+                '}';
+    }
+}
